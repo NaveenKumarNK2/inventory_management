b900757f04d1a287fa11d533f66d3036d2945b65
NaveenKumarNK2 <naveenkumarr722@gmail.com>
2024-06-09 18:53:55 +0530

    first commit login

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,121 @@
+<x-guest-layout>
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+              <!-- First Name -->
+              <div>
+                <x-input-label for="first_name" :value="__('First Name')" />
+                <x-text-input id="first_name" class="block mt-1 w-full" type="text" name="first_name" :value="old('first_name')" required autofocus />
+                <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
+            </div>
+
+            <!-- Last Name -->
+            <div>
+                <x-input-label for="last_name" :value="__('Last Name')" />
+                <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required />
+                <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
+            </div>
+
+          <!-- Date of Birth -->
+          <div>
+                <x-input-label for="dob" :value="__('Date of Birth')" />
+                <x-text-input id="dob" class="block mt-1 w-full" type="date" name="dob" :value="old('dob')" required />
+                <x-input-error :messages="$errors->get('dob')" class="mt-2" />
+            </div>
+
+            <!-- Email Address -->
+            <div class="mt-4">
+                <x-input-label for="email" :value="__('Email')" />
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+
+            <!-- Mobile -->
+            <div class="mt-4">
+                <x-input-label for="mobile" :value="__('Mobile')" />
+                <x-text-input id="mobile" class="block mt-1 w-full" type="text" name="mobile" :value="old('mobile')" required />
+                <x-input-error :messages="$errors->get('mobile')" class="mt-2" />
+            </div>
+
+            <!-- Address -->
+            <div class="mt-4">
+                <x-input-label for="address" :value="__('Address')" />
+                <x-text-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required />
+                <x-input-error :messages="$errors->get('address')" class="mt-2" />
+            </div>
+
+            <!-- State -->
+            <div class="mt-4">
+                <x-input-label for="state" :value="__('State')" />
+                <x-text-input id="state" class="block mt-1 w-full" type="text" name="state" :value="old('state')" required />
+                <x-input-error :messages="$errors->get('state')" class="mt-2" />
+            </div>
+
+            <!-- City -->
+            <div class="mt-4">
+                <x-input-label for="city" :value="__('City')" />
+                <x-text-input id="city" class="block mt-1 w-full" type="text" name="city" :value="old('city')" required />
+                <x-input-error :messages="$errors->get('city')" class="mt-2" />
+            </div>
+
+            <!-- Pincode -->
+            <div class="mt-4">
+                <x-input-label for="pincode" :value="__('Pincode')" />
+                <x-text-input id="pincode" class="block mt-1 w-full" type="text" name="pincode" :value="old('pincode')" required />
+                <x-input-error :messages="$errors->get('pincode')" class="mt-2" />
+            </div>
+
+            <!-- Username -->
+            <div class="mt-4">
+                <x-input-label for="username" :value="__('Username')" />
+                <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required />
+                <x-input-error :messages="$errors->get('username')" class="mt-2" />
+            </div>
+
+        <!-- Name -->
+        <!-- <div>
+            <x-input-label for="name" :value="__('Name')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div> -->
+
+        <!-- Email Address -->
+        <!-- <div class="mt-4">
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div> -->
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="mt-4">
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                            type="password"
+                            name="password_confirmation" required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                {{ __('Already registered?') }}
+            </a>
+
+            <x-primary-button class="ms-4">
+                {{ __('Register') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
